71deb5c658f6715299917024087fdf83162eb633
jiangu <54923374+wujiangu16@users.noreply.github.com>
2024-01-29 21:02:53 +0800

    start rm null

--- plugins/community.lua
+++ plugins/community.lua
@@ -17,7 +17,7 @@ return {
           accept_line = false,
           next = "<C-.>",
           prev = "<C-,>",
-          dismiss = "<C/>",
+          dismiss = "<C-]>",
         },
       },
     },
